0569fdd3b9fa73211d4c8067ebd611e4098cc424
somerandomguy-coder <153010587+somerandomguy-coder@users.noreply.github.com>
2024-08-18 14:12:31 +1000

    process data to the right format to train

--- src/pre-processing.py
+++ src/pre-processing.py
@@ -17,4 +17,26 @@ def tokenize(sentence):
     return tokens
 
 
-# test: print(tokenize("i hAVe a brother, and a sister. <EOS>"))
+assert tokenize("i hAVe a brother, and a sister. <EOS>") == ['i', 'have', 'a', 'brother', ',', 'and', 'a', 'sister', '.', '<EOS>']
+
+def pad_sequences(ins, labs):
+  combine = ins+labs
+  length = [len(sentence) for sentence in combine]
+  max_length = max(length)
+  padded_ins = []
+  padded_labs = []
+  for sequence in ins:
+    padding = [0] * (max_length - len(sequence))
+    padded_sequence = sequence + padding
+    padded_ins.append(padded_sequence)
+  for sequence in labs:
+    padding = [0] * (max_length - len(sequence))
+    padded_sequence = sequence + padding
+    padded_labs.append(padded_sequence)
+  return padded_ins, padded_labs
+
+one, two = pad_sequences([[3,2,4,2,3,2,3], [3,2,2]],
+                         [[3,2],[3,2,1]])
+
+assert len(one) == len(two)
+assert two[1] == [3,2,1,0,0,0,0]
